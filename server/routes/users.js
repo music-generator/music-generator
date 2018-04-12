9a97e68f1b1630fa3ecebc68015e30ebb84b764f
Dani Damara <danny12march@gmail.com>
2018-04-12 20:38:58 +0700

    Sign in and sing up, + login Auth Server Side with mlab

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+const {signIn, signUp} = require('../controllers/user.controller')
+const {loginAuth} = require('../middleware/auth')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+//tes auth udah login apa belum
+router.get('/', loginAuth, function(req, res, next) {
+        res.send('respond with a resource')
+       })
+       .post('/signin', signIn)
+       .post('/signup', signUp)
 
 module.exports = router;
