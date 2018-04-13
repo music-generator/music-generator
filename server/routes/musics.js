8be86fef0909e510f797687f37866981f2e8d92a
Dani Damara <danny12march@gmail.com>
2018-04-13 13:34:50 +0700

    add Middleware sama get data user by ID

--- server/routes/musics.js
+++ server/routes/musics.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const multer = require('multer')
 const {getAll,getOne,add,update,remove,addComment,uploadFile, getLike} =  require('../controllers/music.controller')
 const {sendFileGCS} = require('../middleware/uploadGCS')
+const {loginAuth} = require('../middleware/auth')
 const upload = multer({
   storage: multer.memoryStorage(),
   limits: {
@@ -10,14 +11,14 @@ const upload = multer({
   }
 })
 router.get('/',getAll)
-router.get('/:id',getOne)
-router.post('/add',add)
-router.put('/update/:id',update)
-router.delete('/delete/:id',remove)
-router.post('/addComment/:id',addComment)
-router.put('/getLike/:id',getLike)
+router.get('/:id', loginAuth, getOne)
+router.post('/add', loginAuth, add)
+router.put('/update/:id', loginAuth, update)
+router.delete('/delete/:id', loginAuth, remove)
+router.post('/addComment/:id', loginAuth, addComment)
+router.put('/getLike/:id', loginAuth, getLike)
 // router.post('/upload', upload.single('music'),sendFileGCS, uploadFile)
-router.post('/upload', upload.fields([{name: 'music', maxCount: 1},{name: 'picture', maxCount: 1}]),sendFileGCS, uploadFile)
+router.post('/upload', loginAuth, upload.fields([{name: 'music', maxCount: 1},{name: 'picture', maxCount: 1}]),sendFileGCS, uploadFile)
 
 
 module.exports = router
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,14 +1,13 @@
 var express = require('express')
 var router = express.Router()
-const {signIn, signUp} = require('../controllers/user.controller')
+const {signIn, signUp, findUser} = require('../controllers/user.controller')
 const {loginAuth} = require('../middleware/auth')
 
 /* GET users listing. */
 //tes auth udah login apa belum
-router.get('/', loginAuth, function(req, res, next) {
-        res.send('respond with a resource')
-       })
-       .post('/signin', signIn)
-       .post('/signup', signUp)
+router.get('/', loginAuth, findUser)
+      .post('/signin', signIn)
+      .post('/signup', signUp)
+
 
 module.exports = router;
